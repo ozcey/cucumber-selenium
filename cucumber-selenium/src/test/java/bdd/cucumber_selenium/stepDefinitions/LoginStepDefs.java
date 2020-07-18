ce336711ff948cf6d7c73e637fecf0f45a32252c
Oz <ozcey2018@gmail.com>
2020-07-18 12:52:33 -0400

    created utils functions

--- cucumber-selenium/src/test/java/bdd/cucumber_selenium/stepDefinitions/LoginStepDefs.java
+++ cucumber-selenium/src/test/java/bdd/cucumber_selenium/stepDefinitions/LoginStepDefs.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import bdd.cucumber_selenium.utils.Utils;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
@@ -17,20 +18,25 @@ import io.cucumber.java.en.*;
 public class LoginStepDefs {
 
 	WebDriver driver;
+	Utils utils;
 
 	@Before
 	public void before() {
 		System.setProperty("webdriver.chrome.driver", "F:\\PROGRAMMING\\SDET\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+//		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+//		driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+
+		utils = new Utils();
+//		utils.setUp(driver);
 	}
 
 	@After
 	public void after() {
 		driver.manage().deleteAllCookies();
 		driver.quit();
+//		utils.tearDown(driver);
 	}
 
 	@Given("^User navigates to home page on the website$")
@@ -41,39 +47,39 @@ public class LoginStepDefs {
 	@And("^User clicks on the login button on home page$")
 	public void user_clicks_on_the_login_button_on_home_page() throws Throwable {
 		driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul[2]/li[2]/a")).click();
+		utils.findElelement(driver, "//*[@id=\"navbarSupportedContent\"]/ul[2]/li[2]/a").click();
+
 	}
 
 	@Then("^User should be taken the successful login page$")
 	public void user_should_be_taken_the_successful_login_page() throws Throwable {
-		String text = driver
-				.findElement(By.xpath("/html/body/app-root/section/main/app-login/section/mat-card/mat-card-title"))
-				.getText();
-		assertEquals("Login", text);
+		utils.validateText("Login", utils
+				.findElelement(driver, "/html/body/app-root/section/main/app-login/section/mat-card/mat-card-title")
+				.getText());
 	}
 
 	@And("^User enters a valid username$")
 	public void user_enters_a_valid_username() throws Throwable {
-		driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).sendKeys("markj@gmail.com");
-
+		utils.findElelement(driver, "//*[@id=\"mat-input-0\"]").sendKeys("markj@gmail.com");
 	}
 
 	@And("^User enters a valid password$")
 	public void user_enters_a_valid_password() throws Throwable {
-		driver.findElement(By.xpath("//*[@id=\"mat-input-1\"]")).sendKeys("password");
+		utils.findElelement(driver, "//*[@id=\"mat-input-1\"]").sendKeys("password");
+
 	}
 
 	@When("^User clicks on the login button$")
 	public void user_clicks_on_the_login_button() throws Throwable {
-		driver.findElement(By.xpath("/html/body/app-root/section/main/app-login/section/mat-card/form/button")).click();
+		utils.findElelement(driver, "/html/body/app-root/section/main/app-login/section/mat-card/form/button").click();
 	}
 
 	@Then("^User should be taken the successful customer page$")
 	public void user_should_be_taken_the_successful_customer_page() throws Throwable {
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		wait.until(ExpectedConditions
-				.visibilityOfElementLocated(By.xpath("//*[@id=\"mat-tab-label-0-1\"]/div")));
-		String text = driver.findElement(By.xpath("//*[@id=\"mat-tab-label-0-1\"]/div")).getText();
-		assertEquals("Customer List", text);
+		utils.waitUntil(driver, "//*[@id=\"mat-tab-label-0-1\"]/div");
+		utils.validateText("Customer List",
+				utils.findElelement(driver, "//*[@id=\"mat-tab-label-0-1\"]/div").getText());
+
 	}
 
 }
--- cucumber-selenium/src/test/java/bdd/cucumber_selenium/utils/Utils.java
+++ cucumber-selenium/src/test/java/bdd/cucumber_selenium/utils/Utils.java
@@ -1,5 +1,42 @@
 package bdd.cucumber_selenium.utils;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class Utils {
+
+	public void setUp(WebDriver driver) {
+		System.setProperty("webdriver.chrome.driver", "F:\\PROGRAMMING\\SDET\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+//		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+//		driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+	}
+
+	public void tearDown(WebDriver driver) {
+		driver.manage().deleteAllCookies();
+		driver.quit();
+	}
+
+	public WebElement findElelement(WebDriver driver, String xPath) {
+		return driver.findElement(By.xpath(xPath));
+	}
+
+	public void validateText(String expected, String actual) {
+		assertEquals(expected, actual);
+	}
+
+	public  void waitUntil(WebDriver driver, String xPath) {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xPath)));
+	}
 
 }
